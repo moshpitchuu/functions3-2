346468f81dffc17e1d8fad5bba6ab11ce863b0af
moshpitchuu <v.hardy@mcaabhm.org>
2024-11-19 19:55:35 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,97 @@
+function resetLives () {
+    info.setLife(3)
+    info.setScore(45)
+}
+function calculateAverage () {
+    numOfEntries = game.askForNumber("How many numbers do you want to input?", 1)
+    list = []
+    for (let index = 0; index < 4; index++) {
+        list.push(game.askForNumber("Enter a number", 2))
+    }
+    total = 0
+    for (let value of list) {
+        total += value
+    }
+    game.splash("The average is ", total / numOfEntries)
+}
+function initiateGame () {
+    hero = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(hero)
+    hero.setStayInScreen(true)
+    for (let index = 0; index < 2; index++) {
+        fireball = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 4 4 4 4 4 . . . . . . 
+            . . . 4 4 4 5 5 5 d 4 4 4 4 . . 
+            . . 4 d 5 d 5 5 5 d d d 4 4 . . 
+            . . 4 5 5 1 1 1 d d 5 5 5 4 . . 
+            . 4 5 5 5 1 1 1 5 1 1 5 5 4 4 . 
+            . 4 d d 1 1 5 5 5 1 1 5 5 d 4 . 
+            . 4 5 5 1 1 5 1 1 5 5 d d d 4 . 
+            . 2 5 5 5 d 1 1 1 5 1 1 5 5 2 . 
+            . 2 d 5 5 d 1 1 1 5 1 1 5 5 2 . 
+            . . 2 4 d d 5 5 5 5 d d 5 4 . . 
+            . . . 2 2 4 d 5 5 d d 4 4 . . . 
+            . . 2 2 2 2 2 4 4 4 2 2 2 . . . 
+            . . . 2 2 4 4 4 4 4 4 2 2 . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            `, SpriteKind.Projectile)
+        fireball.setVelocity(randint(-50, 50), randint(-50, 50))
+        fireball.setPosition(randint(10, 150), randint(10, 110))
+        fireball.setBounceOnWall(true)
+    }
+}
+function doSomething (bool: boolean) {
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 4 4 4 4 4 . . . . . . 
+        . . . 4 4 4 5 5 5 d 4 4 4 4 . . 
+        . . 4 d 5 d 5 5 5 d d d 4 4 . . 
+        . . 4 5 5 1 1 1 d d 5 5 5 4 . . 
+        . 4 5 5 5 1 1 1 5 1 1 5 5 4 4 . 
+        . 4 d d 1 1 5 5 5 1 1 5 5 d 4 . 
+        . 4 5 5 1 1 5 1 1 5 5 d d d 4 . 
+        . 2 5 5 5 d 1 1 1 5 1 1 5 5 2 . 
+        . 2 d 5 5 d 1 1 1 5 1 1 5 5 2 . 
+        . . 2 4 d d 5 5 5 5 d d 5 4 . . 
+        . . . 2 2 4 d 5 5 d d 4 4 . . . 
+        . . 2 2 2 2 2 4 4 4 2 2 2 . . . 
+        . . . 2 2 4 4 4 4 4 4 2 2 . . . 
+        . . . . . 2 2 2 2 2 2 . . . . . 
+        `, SpriteKind.Projectile)
+    mySprite.setVelocity(randint(-50, 50), randint(-50, 50))
+    if (bool) {
+        mySprite.setBounceOnWall(true)
+    } else {
+        mySprite.setFlag(SpriteFlag.AutoDestroy, true)
+    }
+}
+let mySprite: Sprite = null
+let fireball: Sprite = null
+let hero: Sprite = null
+let total = 0
+let list: number[] = []
+let numOfEntries = 0
+calculateAverage()
+doSomething(true)
+resetLives()
+initiateGame()
